6a4e59df5161e79a971fc5e7906e092203394a22
Mikael Ahlinder <mikael.ahlinder@aptitud.se>
2017-12-15 09:51:36 +0100

    delete

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "/Users/mikaelahlinder/Code/aptitud.se/src/layouts/index.js"
-  import data from "/Users/mikaelahlinder/Code/aptitud.se/.cache/json/layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
