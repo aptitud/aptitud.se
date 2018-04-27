bc4885dc64df1858e2a24b4b0350a88a0fb99015
Johan Leino <johan.leino@aptitud.se>
2018-04-27 14:04:01 +0200

    border bottom fix

--- src/components/Section.js
+++ src/components/Section.js
@@ -2,20 +2,20 @@ import React from 'react'
 import { css } from 'emotion'
 
 const sectionStyle = css`
-  padding-top: 20px;
-  padding-bottom: 50px;
-  border-bottom: 10px solid #fff;
+    padding-top: 20px;
+    padding-bottom: 50px;
+    border-bottom: 10px solid #fff;
 `
 
 const Section = ({ id, heading, children }) => (
-  <section className={sectionStyle} id={id}>
-    <div className="container row">
-      <div className="col s12 flow-text">
-        <h2>{heading}</h2>
-        {children}
-      </div>
-    </div>
-  </section>
+    <section className={sectionStyle} id={id}>
+        <div className="container row">
+            <div className="col s12 flow-text">
+                <h2>{heading}</h2>
+                {children}
+            </div>
+        </div>
+    </section>
 )
 
 export default Section
