bb4b9d6a665f1c8d3dfe7f116cdc3076a60c304a
Mikael Ahlinder <mikael.ahlinder@aptitud.se>
2019-10-14 20:56:21 +0200

    aptigram

--- aptigram.js
+++ aptigram.js
@@ -2,26 +2,22 @@ const fs = require('fs')
 const request = require('request-promise-native')
 require('dotenv').config()
 
-const run = async (access_token, count = 24) => {
-  await request
-    .get('https://api.instagram.com/v1/users/self/media/recent', {
-      qs: { access_token, count },
-      json: true
-    })
-    .then(({ data }) =>
-      data.map(({ id, images, caption, location }) => ({
-        url: images.low_resolution.url,
-        caption: caption && caption.text,
-        location: location && location.name
-      }))
-    )
-    .then(json => {
-      fs.writeFileSync('src/aptigram.json', JSON.stringify(json, null, 2))
-    })
-    .catch(error => {
-      console.log(error.message)
-      process.exit(1)
-    })
-}
-
-run(process.env.INSTAGRAM_ACCESS_TOKEN, process.env.APTIGRAM_IMAGES)
+request
+  .get('https://api.instagram.com/v1/users/self/media/recent', {
+    qs: { access_token: process.env.INSTAGRAM_ACCESS_TOKEN, count: 24 },
+    json: true
+  })
+  .then(({ data }) =>
+    data.map(({ id, images, caption, location }) => ({
+      url: images.low_resolution.url,
+      caption: caption && caption.text,
+      location: location && location.name
+    }))
+  )
+  .then(json => {
+    fs.writeFileSync('src/aptigram.json', JSON.stringify(json, null, 2))
+  })
+  .catch(error => {
+    console.log(error.message)
+    process.exit(1)
+  })
