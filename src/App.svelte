e0c060052ee350102bfc9ee6db2287037f29422a
Mikael Ahlinder <mikael.ahlinder@aptitud.se>
2020-11-09 15:48:25 +0100

    disable ig

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,25 @@
 <script>
-  import Hero from "./Hero.svelte";
-  import Section from "./Section.svelte";
-  import Fellows from "./Fellows.svelte";
-  import Contact from "./Contact.svelte";
-  import Aptigram from "./Aptigram.svelte";
-  import images from "./aptigram.json";
-  import ctf from "./ctf.json";
+  import Hero from './Hero.svelte'
+  import Section from './Section.svelte'
+  import Fellows from './Fellows.svelte'
+  import Contact from './Contact.svelte'
+  import Aptigram from './Aptigram.svelte'
+  import images from './aptigram.json'
+  import ctf from './ctf.json'
 
-  export let name;
+  export let name
 
-  const [enklare] = ctf.page.filter(p => p.name === "About");
-  const [roligare] = ctf.page.filter(p => p.name === "Aday");
-  const [gladare] = ctf.page.filter(p => p.name === "Fellows");
-  const [aptigram] = ctf.page.filter(p => p.name === "Aptigram");
-  const [kontakt] = ctf.contact;
+  const [enklare] = ctf.page.filter((p) => p.name === 'About')
+  const [roligare] = ctf.page.filter((p) => p.name === 'Aday')
+  const [gladare] = ctf.page.filter((p) => p.name === 'Fellows')
+  const [aptigram] = ctf.page.filter((p) => p.name === 'Aptigram')
+  const [kontakt] = ctf.contact
 </script>
 
 <style>
   :global(body) {
     margin: 0;
-    font-family: "Roboto", sans-serif;
+    font-family: 'Roboto', sans-serif;
     color: white;
   }
   :global(p) {
@@ -34,7 +34,7 @@
     margin: 0;
     font-weight: 300;
     font-size: 2rem;
-    font-family: "Roboto", sans-serif;
+    font-family: 'Roboto', sans-serif;
   }
 </style>
 
@@ -44,10 +44,11 @@
 <Section {...gladare} backgroundColor="#ef6031">
   <Fellows />
 </Section>
+<!--
 <Section header="Aptigram" backgroundColor="#784e90" fullWidth>
   <Aptigram />
 </Section>
-
+-->
 <Section {...{ ...kontakt, image: null }} backgroundColor="#00A69A">
   <Contact {...kontakt} />
 </Section>
